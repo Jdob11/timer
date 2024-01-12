4a868ec9f5da85cc4765e3dd885df59e3b8b5038
Jeff Dobson <jbdobson11@gmail.com>
2024-01-12 10:09:06 -0800

    created command line timer function

---
+++ timer.js
@@ -0,0 +1,11 @@
+const args = process.argv.slice(2);
+
+for (const arg of args) {
+  if (isNaN(+arg) || +arg < 0) {
+    console.log("Your paramters may only include positive numbers");
+    continue;
+  }
+  setTimeout(() => {
+    process.stdout.write('\x07');
+  }, arg * 1000);
+}
