f96f30a9f1ca039daf46ac5c1bb0330081f79bd1
Jdob11 <jbdobson11@gmail.com>
2024-02-15 06:50:49 -0800

    wrote function to take command line numbers to set a timer to beep

--- timer.js
+++ timer.js
@@ -2,10 +2,10 @@ const args = process.argv.slice(2);
 
 for (const arg of args) {
   if (isNaN(+arg) || +arg < 0) {
-    console.log("Your paramters may only include positive numbers");
+    console.log("Your arguments may only include positive numbers");
     continue;
   }
   setTimeout(() => {
     process.stdout.write('\x07');
   }, arg * 1000);
-}
+};
